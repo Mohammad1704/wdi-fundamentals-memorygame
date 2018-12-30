abdc31cd4d09fe7f72c7e139377c98fc3bc9ce2d
mohammad <natgeo1416@hotmail.com>
2018-12-30 17:26:04 +0300

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,8 @@
+console.log("Up and running!");
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
