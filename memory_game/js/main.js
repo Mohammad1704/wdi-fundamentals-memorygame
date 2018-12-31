5308e27fc1ca25baa919af81c4d51bfd34416ec1
mohammad <natgeo1416@hotmail.com>
2018-12-31 20:53:33 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,32 @@
 console.log("Up and running!");
 var cardOne = "queen";
 var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+
+
+var cards = [ "queen", "queen", "king", "king" ];
+var cardsInPlay =[];
+
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2)
+{
+	if (cardsInPlay[0] === cardsInPlay[1] )
+	 {
+		alert("You found a match!");
+	}
+
+	 else
+	  {
+	  	alert("nooope!");
+	 }
+}
+
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+console.log("User flipped " + cardTwo);
+ // console.log(cardOne);
+ // console.log(cardTwo);
