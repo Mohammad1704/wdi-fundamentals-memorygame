a0894c13350bec7327e780cb210c1b01c14986f4
mohammad <natgeo1416@hotmail.com>
2019-01-02 20:20:57 +0300

    creat game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,25 +1,47 @@
 console.log("Up and running!");
 
 
-var cards = [ "queen", "queen", "king", "king" ];
+var cards = [  
+{
+rank : 'queen',
+suit : 'hearts',
+cardImage :  "images/queen-of-hearts.png"
+},
+{
+rank : 'quee',
+suit : 'diamonds',
+cardImage :  "images/queen-of-diamonds.png"
+},
+{
+rank : 'king',
+suit : 'hearts ',
+cardImage :  "images/king-of-hearts.png"
+},
+{
+rank : 'king',
+suit : 'diamonds',
+cardImage :  "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay =[];
-										//cards[] = cards[cardId]
+										
 var flipCard = function(cardId) {
 
-	cardsInPlay.push(cards[cardId]);  //solve: 1- move it befor flipCard function . 2- use alternative https://stackoverflow.com/questions/4156101/push-array-items-into-another-array 
-												// 3- chang to [cardId] or cardId									// or https://www.hostingadvice.com/how-to/javascript-add-to-array/#push
-console.log("User flipped" + cards[cardId])
+	cardsInPlay.push(cards[cardId].rank);  
+console.log(cards[cardId].cardImage )
+console.log(cards[cardId].suit )
+	console.log("User flipped " + cards[cardId].rank )
 
 if (cardsInPlay.length === 2)
 {
 	if (cardsInPlay[0] === cardsInPlay[1] )
 	 {
-		alert("You found a match!");
+		alert(" You found a match! ");
 	}
 
 	 else
 	  {
-	  	alert("nooope!");
+	  	alert("nah!");
 	 }
 }
 
